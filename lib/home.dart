f00093de4552301b6cd5e7ae09bea8ffdd8f8b58
zain <patniz486@gmail.com>
2024-03-27 21:59:12 +0500

    fixed pagview look

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_news/model/news_channel_headlines_model.dart';
 import 'package:flutter_news/view_model/news_view_model.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -18,69 +14,100 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   NewsViewModel newsViewModel = NewsViewModel();
+
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.sizeOf(context).width * 1;
-    final height = MediaQuery.sizeOf(context).height * 1;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            FutureBuilder<NewsChannelHeadlinesModel>(
-              future: newsViewModel.fetchNewsChannelHeadlinesApi(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<NewsChannelHeadlinesModel> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: SpinKitDancingSquare(
-                      size: 20,
-                      color: Colors.amber,
-                    ),
-                  );
-                } else {
-                  return Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Container(
-                      //image holding container
-                      padding: EdgeInsets.all(20),
-                      height: height * .55,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blueGrey),
-                      child: PageView.builder(
-                          allowImplicitScrolling: true,
-                          scrollDirection: Axis.vertical,
-                          itemCount: snapshot.data!.articles!.length,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              alignment: Alignment.topCenter,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.zero,
-                                child: CachedNetworkImage(
-                                  imageUrl: snapshot
-                                      .data!.articles![index].urlToImage
-                                      .toString(),
-                                  fit: BoxFit.cover,
-                                  placeholder: (context, url) => Container(
-                                    child: const SpinKitDancingSquare(
-                                      color: Colors.amber,
+        child: FutureBuilder<NewsChannelHeadlinesModel>(
+          future: newsViewModel.fetchNewsChannelHeadlinesApi(),
+          builder: (BuildContext context,
+              AsyncSnapshot<NewsChannelHeadlinesModel> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: SpinKitDancingSquare(
+                  size: 20,
+                  color: Colors.amber,
+                ),
+              );
+            } else {
+              return PageView.builder(
+                // itemCount: snapshot.data!.articles!.length,
+                itemBuilder: (context, index) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.articles!.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.70,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.blueGrey,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 110),
+                                  child: ClipRRect(
+                                    child: CachedNetworkImage(
+                                      imageUrl: snapshot
+                                          .data!.articles![index].urlToImage
+                                          .toString(),
+                                      fit: BoxFit.scaleDown,
+                                      placeholder: (context, url) =>
+                                          const SpinKitDancingSquare(
+                                        color: Colors.amber,
+                                      ),
+                                      errorWidget: (context, url, error) =>
+                                          const Icon(
+                                        Icons.error,
+                                        color: Colors.red,
+                                      ),
                                     ),
-                                  ),
-                                  errorWidget: (context, url, error) =>
-                                      const Icon(
-                                    Icons.error,
-                                    color: Colors.red,
                                   ),
                                 ),
                               ),
-                            );
-                          }),
-                    ),
-                  ); // Placeholder, replace with appropriate Widget
-                }
-              },
-            ),
-          ],
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 20),
+                                child: Text(
+                                  snapshot.data!.articles![index].title
+                                      .toString(),
+                                  style: GoogleFonts.cormorantInfant(
+                                    textStyle: const TextStyle(
+                                        letterSpacing: 0, color: Colors.white),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 20),
+                                child: Text(
+                                  snapshot.data!.articles![index].author
+                                      .toString(),
+                                  style: GoogleFonts.cormorantInfant(
+                                    textStyle: const TextStyle(
+                                        letterSpacing: 0, color: Colors.white),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            }
+          },
         ),
       ),
     );

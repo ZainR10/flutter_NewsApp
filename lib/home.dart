4e4b34168edf4e426474bb0bbdd7ed35992bc93a
zain <patniz486@gmail.com>
2024-03-27 20:00:39 +0500

    added pageview and removed listview

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_news/model/news_channel_headlines_model.dart';
@@ -21,44 +23,39 @@ class _HomeState extends State<Home> {
     final width = MediaQuery.sizeOf(context).width * 1;
     final height = MediaQuery.sizeOf(context).height * 1;
     return Scaffold(
-      backgroundColor: Colors.indigo[200],
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Center(
-            child: Text(
-          'Home',
-          style: GoogleFonts.cormorantInfant(
-              fontSize: 24, fontWeight: FontWeight.bold),
-        )),
-      ),
-      body: SizedBox(
-          height: height * .36,
-          width: width,
-          child: FutureBuilder<NewsChannelHeadlinesModel>(
-            future: newsViewModel.fetchNewsChannelHeadlinesApi(),
-            builder: (BuildContext context,
-                AsyncSnapshot<NewsChannelHeadlinesModel> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: SpinKitDancingSquare(
-                    size: 20,
-                    color: Colors.amber,
-                  ),
-                );
-              } else {
-                return ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data!.articles!.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Container(
-                              margin: EdgeInsets.all(8),
-                              width: width * .9,
+      body: SafeArea(
+        child: Column(
+          children: [
+            FutureBuilder<NewsChannelHeadlinesModel>(
+              future: newsViewModel.fetchNewsChannelHeadlinesApi(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<NewsChannelHeadlinesModel> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: SpinKitDancingSquare(
+                      size: 20,
+                      color: Colors.amber,
+                    ),
+                  );
+                } else {
+                  return Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                      //image holding container
+                      padding: EdgeInsets.all(20),
+                      height: height * .55,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blueGrey),
+                      child: PageView.builder(
+                          allowImplicitScrolling: true,
+                          scrollDirection: Axis.vertical,
+                          itemCount: snapshot.data!.articles!.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              alignment: Alignment.topCenter,
                               child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20),
+                                borderRadius: BorderRadius.zero,
                                 child: CachedNetworkImage(
                                   imageUrl: snapshot
                                       .data!.articles![index].urlToImage
@@ -76,39 +73,16 @@ class _HomeState extends State<Home> {
                                   ),
                                 ),
                               ),
-                            ),
-                            Positioned(
-                              bottom: 8,
-                              left: 8,
-                              right: 8,
-                              child: Container(
-                                padding: EdgeInsets.fromLTRB(12, 12, 12, 8),
-                                decoration: BoxDecoration(
-                                  // borderRadius: const BorderRadius.only(
-                                  //   bottomLeft: Radius.circular(20),
-                                  //   bottomRight: Radius.circular(20),
-                                  // ),
-                                  color: Colors.black.withOpacity(0.6),
-                                ),
-                                child: Text(
-                                  snapshot.data!.articles![index].title
-                                      .toString(),
-                                  style: GoogleFonts.cormorantInfant(
-                                    textStyle:
-                                        const TextStyle(color: Colors.white),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }); // Placeholder, replace with appropriate Widget
-              }
-            },
-          )),
+                            );
+                          }),
+                    ),
+                  ); // Placeholder, replace with appropriate Widget
+                }
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

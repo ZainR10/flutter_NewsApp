58558b7112006a4de7f16fee112af3e8aae769b3
zain <patniz486@gmail.com>
2024-04-01 18:33:38 +0500

    removed unnecessery imports

--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_news/View/news_detail_screen.dart';
+
 import 'package:flutter_news/model/news_channel_headlines_model.dart';
 import 'package:flutter_news/view_model/news_view_model.dart';
 import 'package:flutter_news/view_model/routes.dart';

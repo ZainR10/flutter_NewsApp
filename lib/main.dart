694b62152244f976118e65f1caeece1c5c192318
zain <patniz486@gmail.com>
2025-01-05 23:14:14 +0500

    upadated readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/View/splash_screen.dart';
 import 'package:flutter_news/view_model/news_channel_provider.dart';
 import 'package:flutter_news/view_model/themeprovider.dart';
 import 'package:flutter_news/view_model/bottom_navbar_provider.dart';
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           initialRoute: RoutesName.splashScreen,
           onGenerateRoute: Routes.generateRoute,
+          // home: SplashScreen(),
         ));
   }
 }
